3a8cfff5657fb1cdc2f8a9e2a2d08844f31e6590
MihiranWijesekara <achinthamihiran654@gamil.com>
2024-07-04 02:55:53 +0530

    Your commit message

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dashboard_ui1/dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Dashboard(),
+    );
+  }
+}
+//12.09
